486a5a719310e775d51c421b5bc3d8aa177b9e3d
Aminata bella <baminatabella@gmail.com>
2023-01-25 18:53:46 +0000

    update manifest version

--- __manifest__.py
+++ __manifest__.py
@@ -2,7 +2,7 @@
 
 {
     'name': 'Système de facturation et Customisation',
-    'version': '1.0',
+    'version': '1.1',
     'author': 'Bella Bah',
     'website': 'https://www.aminatabella.com',
     'support': 'baminatabella@gmail.com',
